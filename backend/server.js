45fece78738768cd4a764008e54a3f574f4ce99f
tochukwu <okeakputochukwu9@gmail.com>
2026-04-23 10:35:14 -0400

    task controller error fixed

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,9 @@ const app = require("./app");
 
 const PORT = process.env.PORT || 5000;
 
+console.log("🔥 SERVER STARTED");
+console.log("MONGO_URL:", process.env.MONGO_URL);
+
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
