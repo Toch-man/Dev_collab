c5131bfbbf2ce260f9a0305318dc78067f9befd6
tochukwu <okeakputochukwu9@gmail.com>
2026-04-27 06:33:14 -0400

    invite routes added

---
+++ backend/routes/invite_routes.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+const { verify_token } = require("../middleware/auth");
+const invite_controller = require("../controllers/inviteController");
+
+router.get("/get_my_invites", verify_token, invite_controller.get_my_invites);
+
+router.post("/accept_invite", verify_token, invite_controller.accept_invite);
+
+router.post("reject_invite", verify_token, invite_controller.reject_invite);
+
+module.exports = router;
